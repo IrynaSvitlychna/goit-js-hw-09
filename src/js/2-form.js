5bcda3359385c3c6a62c6f8a68f09d38a1a4b099
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-01-13 13:44:04 +0200

    fixed buges from mentor

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -8,7 +8,7 @@ formListener.addEventListener("input", (event) => {
 
    obj[event.target.name] = event.target.value.trim();
 
- 
+  
  localStorage.setItem(localStorageKey, JSON.stringify(obj));
 }
  );
@@ -17,29 +17,27 @@ formListener.addEventListener("input", (event) => {
     const savedData = localStorage.getItem("feedback-form-state");
     if(savedData) {
       const parsedData = JSON.parse(savedData);
-         input.value = parsedData.email;
-         textarea.value = parsedData.message;
-
-         //        if(inputElement) {
-         //    inputElement.value = parsedData[key] || {};
-         //  }
-        }
+      const {email, message} = formListener.elements;
+      if (email && message) {
+        email.value = parsedData.email || "";
+        message.value = parsedData.message || "";
+      }
+        
+                }
       }
     );
-//  const formValue = JSON.parse(localStorage.getItem(localStorageKey));
+
 
 
  formListener.addEventListener("submit", evt => {
     evt.preventDefault();
     const { email, message } = evt.target.elements;
     if (email.value === "" || message.value === "") {
-        alert `All form fields must be filled in`;
+        alert ("All form fields must be filled in");
           }  else {
             console.log(obj);
             localStorage.removeItem(localStorageKey);
-
              formListener.reset();
           }
-    
   });
 
