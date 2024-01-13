99336f6fab58845f292283ecd20c3ca239ed6b54
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-01-13 13:58:59 +0200

    fixed code

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -21,11 +21,9 @@ formListener.addEventListener("input", (event) => {
       if (email && message) {
         email.value = parsedData.email || "";
         message.value = parsedData.message || "";
-      }
-        
-                }
-      }
-    );
+      }    
+    }
+  });
 
 
 
